6ae83ad34b2386d2aa6c2de6c9c617eb32d551c4
Ashle3 <ashleeb3@byui.edu>
2023-05-20 12:45:49 -0500

    Added the option list

--- ConsoleAppC#/Program.cs
+++ ConsoleAppC#/Program.cs
@@ -8,10 +8,22 @@ namespace ConsoleAppC_
 {
     internal class Program
     {
+        //This method/function prints a list of options every time it's called
+        static void PrintOptions()
+        {
+            Console.WriteLine("1. Add item");
+            Console.WriteLine("2. View Cart");
+            Console.WriteLine("3. Remove item");
+            Console.WriteLine("4. Leave");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("First C# Program, New Change");
-            Console.ReadKey();
+            Console.WriteLine("Welcome to the Grocery Store!");
+            Console.WriteLine();
+            Console.WriteLine("Enter the number of the option you would like to perform: ");
+            Console.WriteLine();
+            PrintOptions();
+            Console.ReadLine();
         }
     }
 }
